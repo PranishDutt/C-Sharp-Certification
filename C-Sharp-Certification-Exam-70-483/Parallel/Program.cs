5412e53dc10695c740c5d8d03c797f0cd3720e34
PranishDutt <pranishdutt@hotmail.com>
2015-04-19 03:46:31 -0700

    LINQ, P-LINQ, and Method Syntax vs. Query Syntax.

--- C-Sharp-Certification-Exam-70-483/Parallel/Program.cs
+++ C-Sharp-Certification-Exam-70-483/Parallel/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Helpers;
 
@@ -37,12 +38,41 @@ namespace ParallelMethods
 
 			ActionTimer.Time("Parallel.Invoke", () => Parallel.Invoke(iterationActions));
 
+			// P-LINQ Setup
+			iterations = 100000;
+			List<int> dataSet = Enumerable.Range(1, iterations).ToList();
+
+			// LINQ (Non-Parallelized) Method Syntax
+			ActionTimer.Time("LINQ (Non-Parallelized) Method Syntax", () => dataSet.Where(value => value % 2 == 0).ToList());
+
+			// LINQ (Non-Parallelized) Query Syntax
+			ActionTimer.Time("LINQ (Non-Parallelized) Query Syntax", () => 
+				{
+					(from value in dataSet
+					 where value % 2 == 0
+					 select value).ToList();
+				});
+
+			// P-LINQ Method Syntax
+			ActionTimer.Time("P-LINQ Method Syntax", () => dataSet.AsParallel().Where(value => value % 2 == 0).ToList());
+
+			// P-LINQ Query Syntax
+			ActionTimer.Time("P-LINQ Query Syntax", () => 
+					(from value in dataSet.AsParallel()
+					 where value % 2 == 0
+					 select value).ToList()
+				);
+
+			// LINQ ForEach
+			ActionTimer.Time("LINQ ForEach", () => dataSet.ForEach((value) => { value = value / 2; }));
+
+			// P-LINQ ForAll
+			ActionTimer.Time("P-LINQ ForAll", () => dataSet.AsParallel().ForAll((value) => { value = value / 2; }));
+
 			// Disclaimer
-			Console.WriteLine(Environment.NewLine + "Disclaimer: Subsequent Parallel method calls may run faster due to memory caching.");
+			Console.WriteLine(Environment.NewLine + "Disclaimer:" + Environment.NewLine + "Subsequent Parallel method calls may run faster due to memory caching.");
 
 			Console.ReadLine();
 		}
-
-
 	}
 }
