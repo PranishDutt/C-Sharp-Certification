45f904d76d22d98acd2bb51ac33d432d1976a339
PranishDutt <pranishdutt@hotmail.com>
2015-04-26 12:46:50 -0700

    Query syntax for LINQ Projection.

--- C-Sharp-Certification-Exam-70-483/LINQConcepts/Program.cs
+++ C-Sharp-Certification-Exam-70-483/LINQConcepts/Program.cs
@@ -17,16 +17,28 @@ namespace LINQConcepts
 			pseudoData.ForEach(data => Console.WriteLine(data.State + " " + data.City));
 			Console.WriteLine();
 
-			// Projection Example 1
-			Console.WriteLine("Projected City Data:");
+			// Projection Example 1 - Method Syntax
+			Console.WriteLine("Projected City Data - Method Syntax:");
 			List<string> pseudoCityData = pseudoData.Select(data => data.City).ToList();
 			pseudoCityData.ForEach(cityData => Console.WriteLine(cityData));
 			Console.WriteLine();
 
-			// Projection Example 2 (Anonymous Type Projection)
-			Console.WriteLine("Projected Anonymous State & City Data:");
+			// Projection Example 1 - Query Syntax
+			Console.WriteLine("Projected City Data - Query Syntax:");
+			List<string> pseudoCityDataQuerySyntax = (from data in pseudoData select data.City).ToList();
+			pseudoCityDataQuerySyntax.ForEach(cityData => Console.WriteLine(cityData));
+			Console.WriteLine();
+
+			// Projection Example 2 - Method Syntax (Anonymous Type Projection)
+			Console.WriteLine("Projected Anonymous State & City Data - Method Syntax:");
 			var pseudoStateCityDataCollection = pseudoData.Select(data => new { CityAndState = data.City + ", " + data.State });
 			pseudoStateCityDataCollection.ToList().ForEach(data => Console.WriteLine(data.CityAndState));
+			Console.WriteLine();
+
+			// Projection Example 2 - Query Syntax (Anonymous Type Projection)
+			Console.WriteLine("Projected Anonymous State & City Data - Query Syntax:");
+			var pseudoStateCityDataCollectionQuerySyntax = (from data in pseudoData select new { CityAndState = data.City + ", " + data.State });
+			pseudoStateCityDataCollectionQuerySyntax.ToList().ForEach(data => Console.WriteLine(data.CityAndState));
 			Console.WriteLine();
 
 			Console.ReadLine();
